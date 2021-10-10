48fd83a0b23e06e10c68a292547808b4dae2a4ca
PaulTy <paultych@gmail.com>
2021-10-10 09:25:01 +0300

    HomeWork 6 task 30

--- NameParserTest.java
+++ NameParserTest.java
@@ -12,3 +12,11 @@ public class NameParser {
     }
 }
 
+class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
